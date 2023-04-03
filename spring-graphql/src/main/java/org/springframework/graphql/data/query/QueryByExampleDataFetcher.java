b2082e16893706b5742bac9087f3a749eaaf35ad
Kevin McCarpenter™️ <kevin@moderne.io>
2023-04-03 17:42:20 +0000

    refactor: Common static analysis issues
    Co-authored-by: Moderne <team@moderne.io>

--- spring-graphql/src/main/java/org/springframework/graphql/data/query/QueryByExampleDataFetcher.java
+++ spring-graphql/src/main/java/org/springframework/graphql/data/query/QueryByExampleDataFetcher.java
@@ -94,7 +94,7 @@ import org.springframework.validation.BindException;
  */
 public abstract class QueryByExampleDataFetcher<T> {
 
-	private final static Log logger = LogFactory.getLog(QueryByExampleDataFetcher.class);
+	private static final Log logger = LogFactory.getLog(QueryByExampleDataFetcher.class);
 
 
 	private final TypeInformation<T> domainType;
--- spring-graphql/src/main/java/org/springframework/graphql/server/webmvc/GraphQlWebSocketHandler.java
+++ spring-graphql/src/main/java/org/springframework/graphql/server/webmvc/GraphQlWebSocketHandler.java
@@ -209,7 +209,7 @@ public class GraphQlWebSocketHandler extends TextWebSocketHandler implements Sub
 					logger.debug("Executing: " + request);
 				}
 				this.graphQlHandler.handleRequest(request)
-						.flatMapMany((response) -> handleResponse(session, request.getId(), response))
+						.flatMapMany(response -> handleResponse(session, request.getId(), response))
 						.publishOn(state.getScheduler()) // Serial blocking send via single thread
 						.subscribe(new SendMessageSubscriber(id, session, state));
 				return;
@@ -278,7 +278,7 @@ public class GraphQlWebSocketHandler extends TextWebSocketHandler implements Sub
 			// Subscription
 			responseFlux = Flux.from((Publisher<ExecutionResult>) response.getData())
 					.map(ExecutionResult::toSpecification)
-					.doOnSubscribe((subscription) -> {
+					.doOnSubscribe(subscription -> {
 							Subscription prev = getSessionInfo(session).getSubscriptions().putIfAbsent(id, subscription);
 							if (prev != null) {
 								throw new SubscriptionExistsException();
@@ -293,18 +293,18 @@ public class GraphQlWebSocketHandler extends TextWebSocketHandler implements Sub
 		return responseFlux
 				.map(responseMap -> encode(GraphQlWebSocketMessage.next(id, responseMap)))
 				.concatWith(Mono.fromCallable(() -> encode(GraphQlWebSocketMessage.complete(id))))
-				.onErrorResume((ex) -> {
+				.onErrorResume(ex -> {
 					if (ex instanceof SubscriptionExistsException) {
 						CloseStatus status = new CloseStatus(4409, "Subscriber for " + id + " already exists");
 						GraphQlStatus.closeSession(session, status);
 						return Flux.empty();
 					}
-					List<GraphQLError> errors = ((ex instanceof SubscriptionPublisherException) ?
+					List<GraphQLError> errors = (ex instanceof SubscriptionPublisherException) ?
 							((SubscriptionPublisherException) ex).getErrors() :
 							Collections.singletonList(GraphqlErrorBuilder.newError()
 									.message("Subscription error")
 									.errorType(ErrorType.INTERNAL_ERROR)
-									.build()));
+									.build());
 					return Mono.just(encode(GraphQlWebSocketMessage.error(id, errors)));
 				});
 	}
@@ -490,7 +490,7 @@ public class GraphQlWebSocketHandler extends TextWebSocketHandler implements Sub
 	}
 
 
-	private static class WebMvcSessionInfo implements WebSocketSessionInfo {
+	private static final class WebMvcSessionInfo implements WebSocketSessionInfo {
 
 		private final WebSocketSession session;
 
